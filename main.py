3960b3989c4ae3390d40772a4730fec6b5c84327
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2021-02-28 00:31:35 +0300

    Добавление магазина и изменение кнопок

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ display_inf = pygame.display.Info()
 size = w, h = display_inf.current_w, display_inf.current_h
 screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 
+spels = []
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -27,13 +29,68 @@ def load_image(name, colorkey=None):
     return image
 
 
+class Shooop():
+    def __init__(self):
+        self.groups = [pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()]
+        self.page = 1
+        self.buttons = [Button((w // 2 - Button.image_b.get_width() // 2 - Button.image_b.get_width() // 2, h // 8),
+                               self.change_page, 'Spells', self.groups[2]),
+                        Button((w // 2 - Button.image_b.get_width() // 2 + Button.image_b.get_width() // 2, h // 8),
+                               self.change_page, 'Emojy',  self.groups[2])
+            ]
+        self.page_1_buttons = [Button(
+                (w // 2 - Button.image_b.get_width() // 2 + Button.image_b.get_width() // 2 - w // 6 * 2, h // 8 * 2),
+                self.change_page, 'Hit figure ---------------------- 30', self.groups[0],
+                (w // 6 * 4, Button.image_b.get_height())),
+                        Button((w // 2 - Button.image_b.get_width() // 2 + Button.image_b.get_width() // 2 - w // 6 * 2,
+                                h // 8 * 3),
+                               self.change_page, 'Repair figure ---------------------- 500', self.groups[0],
+                               (w // 6 * 4, Button.image_b.get_height()))]
+        self.page_2_buttons = [Button(
+                (w // 2 - Button.image_b.get_width() // 2 + Button.image_b.get_width() // 2 - w // 6 * 2, h // 8 * 2),
+                self.change_page, 'Rat emojy ---------------------- 40', self.groups[1],
+                (w // 6 * 4, Button.image_b.get_height()))]
+
+    def change_page(self, n):
+        self.page = -self.page
+
+    def buy(self, n):
+        pass
+
+    def update(self, args):
+        for i in self.buttons:
+            i.update(args)
+        if self.page == 1:
+            for i in self.page_1_buttons:
+                i.update(args)
+        else:
+            for i in self.page_2_buttons:
+                i.update(args)
+
+    def render(self):
+        self.groups[2].draw(screen)
+        for i in self.buttons:
+            i.rendel()
+        if self.page == 1:
+            self.groups[0].draw(screen)
+            for i in self.page_1_buttons:
+                i.rendel()
+        else:
+            self.groups[1].draw(screen)
+            for i in self.page_2_buttons:
+                i.rendel()
+
 class Button(pygame.sprite.Sprite):
     image_b = load_image('but.png')
     image_k = load_image('but1.png')
 
-    def __init__(self, pos, met, txt):
+    def __init__(self, pos, met, txt, all_sprites1, scale=(1, 1)):
         super().__init__(all_sprites1)
-        self.image = Button.image_b
+        self.scale = scale
+        if scale != (1, 1):
+            self.image = pygame.transform.scale(Button.image_b, scale)
+        else:
+            self.image = Button.image_b
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
@@ -43,18 +100,23 @@ class Button(pygame.sprite.Sprite):
     def update(self, args):
         if args.type == pygame.MOUSEMOTION:
             if self.rect.collidepoint(args.pos):
-                self.image = Button.image_k
+                if self.scale != (1, 1):
+                    self.image = pygame.transform.scale(Button.image_k, self.scale)
+                else:
+                    self.image = Button.image_k
             else:
-                self.image = Button.image_b
+                if self.scale != (1, 1):
+                    self.image = pygame.transform.scale(Button.image_b, self.scale)
+                else:
+                    self.image = Button.image_b
         elif args.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args.pos) and args:
             self.zhamk(0)
 
     def rendel(self):
-        font = pygame.font.Font('16881.otf', 35
-                                )
+        font = pygame.font.Font('16881.otf', 45)
         text = font.render(self.txt, True, pygame.Color('white'))
-        text_x = self.rect.x + Button.image_b.get_width() // 2 - text.get_width() // 2
-        text_y = self.rect.y + Button.image_b.get_height() // 2 - text.get_height() // 2
+        text_x = self.rect.x + self.image.get_width() // 2 - text.get_width() // 2
+        text_y = self.rect.y + self.image.get_height() // 2 - text.get_height() // 2
         screen.blit(text, (text_x, text_y))
 
     def zhamk(self, n):
@@ -66,37 +128,52 @@ def star(n):
     state = 1
 
 
+def shop(n):
+    global state
+    state = 2
+
+
 if __name__ == '__main__':
     # Глобальные штуки
     running = True
     all_sprites1 = pygame.sprite.Group()
     all_sprites = pygame.sprite.Group()
 
+
     # Игровые элементы
     board = BoardNacl()
+    shoop = Shooop()
     generate_level(load_level('pole.txt'), all_sprites, board)
 
     # Элементы меню
-    ext = Button((w // 2 - Button.image_b.get_width() // 2, h // 2), exit, 'EXIT')
-    start = Button((w // 2 - Button.image_b.get_width() // 2, h // 8 * 3), star, 'START')
+    shp = Button((w // 2 - Button.image_b.get_width() // 2, h // 8 * 3), shop, 'SHOP', all_sprites1)
+    ext = Button((w // 2 - Button.image_b.get_width() // 2, h // 2), exit, 'EXIT', all_sprites1)
+    start = Button((w // 2 - Button.image_b.get_width() // 2, h // 8 * 2), star, 'START', all_sprites1)
 
     while running:
         screen.fill((0, 0, 0))
         if state == 0:
             all_sprites1.draw(screen)
             ext.rendel()
+            shp.rendel()
             start.rendel()
         elif state == 1:
             board.render(screen)
             all_sprites.draw(screen)
+        elif state == 2:
+
+            shoop.render()
         pygame.display.flip()
         for event in pygame.event.get():
             if state == 0:
+                shp.update(event)
                 start.update(event)
                 ext.update(event)
             elif state == 1:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     board.get_click(event.pos)
+            elif state == 2:
+                shoop.update(event)
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
